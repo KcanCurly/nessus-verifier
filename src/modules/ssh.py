2d32b9d1b863041b2ea578c68534316b5aea97da
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-05 14:56:47 +0300

    ssh 12

--- src/modules/ssh.py
+++ src/modules/ssh.py
@@ -18,7 +18,7 @@ def check(directory_path, hosts = "hosts.txt"):
         
         try:
             # Execute the command and capture the output
-            result = subprocess.run(command, text=True, capture_output=True, check=True)
+            result = subprocess.run(command, text=True, capture_output=True, shell=True)
             print(result.stdout)
             first_line = result.stdout.splitlines()[0]
             first_word = first_line.split()[0]
@@ -29,16 +29,6 @@ def check(directory_path, hosts = "hosts.txt"):
         except Exception as e:
             # Handle errors (e.g., if the host is unreachable)
             print(f"{e}")
-            print(result.stdout)
-            try:
-                
-                first_line = result.stdout.splitlines()[0]
-                first_word = first_line.split()[0]
-            
-                # Print the output of the command
-                print(f"{host}: {first_word}")
-            except Exception as e:
-                print(f"{e}")
     
 
 def main():
