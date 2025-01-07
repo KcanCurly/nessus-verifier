ec81db33e6a33eb29ed74cc27bdf6286c56cb862
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-07 21:17:01 +0300

    ftp 22

--- src/modules/ftp.py
+++ src/modules/ftp.py
@@ -8,29 +8,130 @@ import subprocess
 import re
 
 anon = []
-sslv2 = []
-sslv3 = []
-tls10 = []
-tls11 = []
 weak_versions = {}
 weak_ciphers = {}
+creds = [
+"anonymous:anonymous"
+"root:rootpasswd"
+"root:12hrs37"
+"ftp:b1uRR3"
+"admin:admin"
+"localadmin:localadmin"
+"admin:1234"
+"apc:apc"
+"admin:nas"
+"Root:wago"
+"Admin:wago"
+"User:user"
+"Guest:guest"
+"ftp:ftp"
+"admin:password"
+"a:avery"
+"admin:123456"
+"adtec:none"
+"admin:admin12345"
+"none:dpstelecom"
+"instrument:instrument"
+"user:password"
+"root:password"
+"default:default"
+"admin:default"
+"nmt:1234"
+"admin:Janitza"
+"supervisor:supervisor"
+"user1:pass1"
+"avery:avery"
+"IEIeMerge:eMerge"
+"ADMIN:12345"
+"beijer:beijer"
+"Admin:admin"
+"admin:1234"
+"admin:1111"
+"root:admin"
+"se:1234"
+"admin:stingray"
+"device:apc"
+"apc:apc"
+"dm:ftp"
+"dmftp:ftp"
+"httpadmin:fhttpadmin"
+"user:system"
+"MELSEC:MELSEC"
+"QNUDECPU:QNUDECPU"
+"ftp_boot:ftp_boot"
+"uploader:ZYPCOM"
+"ftpuser:password"
+"USER:USER"
+"qbf77101:hexakisoctahedron"
+"ntpupdate:ntpupdate"
+"sysdiag:factorycast@schneider"
+"wsupgrade:wsupgrade"
+"pcfactory:pcfactory"
+"loader:fwdownload"
+"test:testingpw"
+"webserver:webpages"
+"fdrusers:sresurdf"
+"nic2212:poiuypoiuy"
+"user:user00"
+"su:ko2003wa"
+"MayGion:maygion.com"
+"admin:9999"
+"PlcmSpIp:PlcmSpIp"
+]
+
+def bruteforce(host):
+    ip = host
+    port = 21
+    if ":" in host:
+        ip = host.split(":")[0]
+        port  = int(host.split(":")[1])
+    host = ip + ":" + str(port)
+    for cred in creds:
+        username = cred.split(":")[0]
+        password = cred.split(":")[1]
+        ftp = FTP()
+        ftp.connect(ip, port)
+        try:
+            l = ftp.login(username, password)
+            if "230" in l:
+                print(f"[+] {host} => {username}:{password}")
+        except Error as e:
+            if "must use encryption" in str(e):
+                ftp = FTP_TLS()
+                ftp.connect(ip, port)
+                try:
+                    l = ftp.login()
+                    if "230" in l:
+                        print(f"[+] {host} => {username}:{password}")
+                except error_perm as ee:
+                    continue
+                except Error as eee:
+                    continue
+    
+        
+    
 
 def check(directory_path, hosts = "hosts.txt"):
     with open(os.path.join(directory_path, hosts), "r") as file:
         
         hosts = [line.strip() for line in file if line.strip()] 
         
+        
+    # Anon    
     for host in hosts:
         ip = host
         port = 21
         if ":" in host:
             ip = host.split(":")[0]
             port  = int(host.split(":")[1])
+        host = ip + ":" + str(port)
         ftp = FTP()
         ftp.connect(ip, port)
         try:
             l = ftp.login()
-            anon.append(host)
+            if "230" in l:
+                anon.append(host)
+
         except Error as e:
             if "must use encryption" in str(e):
                 ftp = FTP_TLS()
@@ -49,8 +150,8 @@ def check(directory_path, hosts = "hosts.txt"):
         print("Anonymous Access on Hosts:")               
         for a in anon:
             print(f"\t{a}")
-            
-                
+
+    # Check TLS      
     for host in hosts:
         ip = host
         port = "21"
@@ -102,22 +203,30 @@ def check(directory_path, hosts = "hosts.txt"):
                     if host not in weak_ciphers:
                         weak_ciphers[host] = []
                     weak_ciphers[host].append(re.sub(r'^\x1b\[[0-9;]*m', '', cipher))
-                    # if cipher.startswith("^"):
-                    #    weak_ciphers[host].append(cipher[6:])
-                    # else: weak_ciphers[host].append(cipher)
+
       
-    if len(weak_ciphers) > 0:              
-        print("Vulnerable TLS Cipher on Hosts:")                
+    if len(weak_ciphers) > 0:       
+        print()       
+        print("Vulnerable TLS Ciphers on Hosts:")                
         for key, value in weak_ciphers.items():
-            print(f"\n{key} - {", ".join(value)}")
+            print(f"\t{key} - {", ".join(value)}")
     
-    if len(weak_versions) > 0:              
-        print("Vulnerable TLS Version on Hosts:")                
+    
+    if len(weak_versions) > 0: 
+        print()             
+        print("Vulnerable TLS Versions on Hosts:")                
         for key, value in weak_versions.items():
-            print(f"\n{key} - {", ".join(value)}")
+            print(f"\t{key} - {", ".join(value)}")
             
-                
-        
+    # bruteforce
+    for host in hosts:
+        ip = host
+        port = "21"
+        if ":" in host:
+            ip = host.split(":")[0]
+            port  = host.split(":")[1]
+    
+            
 
 def main():
     parser = argparse.ArgumentParser(description="FTP module of nessus-verifier.")
