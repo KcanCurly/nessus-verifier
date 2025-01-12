3dddb27ade0ccf9c0dcc6f28a7d14bcfbf97a6fb
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-12 16:45:47 +0300

    smtp 36

--- src/modules/smtp.py
+++ src/modules/smtp.py
@@ -23,9 +23,7 @@ def tls(directory_path, config, hosts = "hosts.txt"):
         command = ["sslscan", "--starttls-smtp", "-no-fallback", "--no-renegotiation", "--no-group", "--no-check-certificate", "--no-heartbleed", "--iana-names", "--connect-timeout=3", host]
         result = subprocess.run(command, text=True, capture_output=True)
         if "Connection refused" in result.stderr or "enabled" not in result.stdout:
-            print(1)
             if "did not appear to be an SMTP service" in result.stderr:
-                print(2)
                 # If TLS is enforced but not thru starttls we can get this error, so in this case we have to do the same command without starttls
                 command = ["sslscan", "-no-fallback", "--no-renegotiation", "--no-group", "--no-check-certificate", "--no-heartbleed", "--iana-names", "--connect-timeout=3", host]
                 result = subprocess.run(command, text=True, capture_output=True)
@@ -142,7 +140,7 @@ def open_relay(directory_path, config, hosts = "hosts.txt"):
             if f"{ip}:{port}" not in vuln:
                 vuln[f"{ip}:{port}"] = []
             vuln[f"{ip}:{port}"].append(tag)
-        except TimeoutError as t: # It could be that server requires TLS/SSL so we need to connect again with TLS
+        except smtplib.SMTPServerDisconnected as t: # It could be that server requires TLS/SSL so we need to connect again with TLS
             print("Timeout: ", t)
             try:
                 smtp = smtplib.SMTP_SSL(ip, port, timeout=5)
