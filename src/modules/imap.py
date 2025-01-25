1ae222490027ef1406aefbe9b04cc482f9eacc22
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-25 13:13:56 +0300

    imap 5

--- src/modules/imap.py
+++ src/modules/imap.py
@@ -6,24 +6,43 @@ import subprocess
 import re
 import imaplib
 import socket
+import ssl
 from src.utilities import get_hosts_from_file
 
 def check(directory_path, config, args, hosts):
     hosts = get_hosts_from_file(hosts)
+    tls_enabled = []
     vuln = []
-    
+    tls_not_forced = []
     for host in hosts:
         try:
             ip = host.split(":")[0]
             port = host.split(":")[1]
             
             mail = imaplib.IMAP4_SSL(ip, int(port))
-            print(f"SSL Connection to {ip}:{port} was a success")
-        except Exception as e: print(type(e))
+            tls_enabled.append(host)
+
+        except ssl.SSLError: vuln.append(host)
+        except Exception:pass
+    
+    for host in tls_enabled:
+        try:
+            ip = host.split(":")[0]
+            port = host.split(":")[1]
+            
+            mail = imaplib.IMAP4(ip, int(port))
+            tls_not_forced.append(host)
+
+        except Exception:pass
     
     if len(vuln) > 0:
-        print("NTP service monlist enabled on hosts:")
+        print("TLS NOT enabled on hosts:")
         for v in vuln:
+            print(f"\t{v}")
+    
+    if len(tls_not_forced) > 0:
+        print("TLS is enabled but NOT forced on hosts:")
+        for v in tls_not_forced:
             print(f"\t{v}")
         
 
