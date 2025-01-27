9c2e46e268526a3686c61c6c50d716bec7b6d9e9
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-27 22:01:11 +0300

    smb

--- src/services/smb.py
+++ src/services/smb.py
@@ -38,7 +38,7 @@ def check(directory_path, config, args, hosts):
             for s in shares:
                 try:
                     print(s['shi1_netname'][:-1])
-                    conn.connectTree(s['shi1_netname'])
+                    conn.connectTree(s['shi1_netname'][:-1])
                     conn.listPath(s['shi1_netname'][:-1], "/")
                     if host not in guess_vuln:
                         guess_vuln[host] = []
