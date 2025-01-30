11ab2dea56c5689ed1e1e4d401b65fecc0020ee1
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-31 00:18:56 +0300

    solver

--- src/solvers/solver.py
+++ src/solvers/solver.py
@@ -9,7 +9,11 @@ def main():
     # 1
     parser_task1 = subparsers.add_parser("1", help="TLS Misconfigurations (Version and Ciphers)")
     parser_task1.add_argument("-f", "--file", type=str, required=True, help="Host file name")
-    parser_task1.add_argument("--age", type=int, required=True, help="Age for task1")
     parser_task1.set_defaults(func=tls.entry_solver)
     
     args = parser.parse_args()
+    
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
