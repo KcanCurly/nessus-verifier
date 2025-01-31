07d99ef0146f927ad2cb9a61c29e895547ef3673
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-01-31 20:33:35 +0300

    smb

--- src/services/smb.py
+++ src/services/smb.py
@@ -96,7 +96,7 @@ def sign_check(hosts: list[str], verbose = False, debug = False):
         port = host.split(":")[1]
         
         try:
-            conn = SMBConnection(ip, ip, timeout=3)
+            conn = SMBConnection(ip, ip, sess_port=int(port),timeout=3)
             if not conn._SMBConnection.is_signing_required():
                 sign.append(host)
         except Exception:pass
@@ -115,7 +115,7 @@ def smbv1_check(hosts: list[str], verbose = False, debug = False):
         ip = host.split(":")[0]
         port = host.split(":")[1]
         try:
-            SMBConnection(host, host, timeout=3, preferredDialect="NT LM 0.12") 
+            SMBConnection(ip, ip, sess_port=int(port), timeout=3, preferredDialect="NT LM 0.12") 
             smbv1.append(host)
         except Exception:pass
     
