2cad4106fd51ec5b553ed3912fdd32c9780ffe29
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-02 19:48:20 +0300

    python

---
+++ src/solvers/python.py
@@ -0,0 +1,42 @@
+from src.utilities.utilities import find_scan, get_header_from_url
+from src.modules.vuln_parse import GroupNessusScanOutput
+from src.utilities import logger
+import re
+
+def helper_parse(subparser):
+    parser_task1 = subparser.add_parser("23", help="Python Unsupported Version")
+    parser_task1.add_argument("-f", "--file", type=str, required=True, help="JSON file name")
+    parser_task1.set_defaults(func=solve) 
+
+def solve(args):
+    versions = {}
+    
+    l= logger.setup_logging(args.verbose)
+    scan: GroupNessusScanOutput = find_scan(args.file, 23)
+    if not scan: 
+        print("No id found in json file")
+        return
+    
+    r = r"Python/(.*)"
+    
+    hosts = scan.hosts
+    for host in hosts:
+        header = get_header_from_url(host, "Server")
+        if header:
+            m = re.search(r, header)
+            if m:
+                m = m.group(1)
+                if " " in m:
+                    m = m.split()[0]
+                m = "Python " + m
+                if m not in versions:
+                    versions[m] = set()
+                versions[m].add(host)
+
+    
+    if len(versions) > 0:
+        print("Detected Python Versions:")
+        for key, value in versions.items():
+            print(f"{key}:")
+            for v in value:
+                print(f"\t{v}")
