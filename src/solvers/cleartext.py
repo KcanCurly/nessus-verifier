a7a8fcd0238dd12875b7d0062f2c1d10a474abef
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-11 23:54:57 +0300

    cleartext

--- src/solvers/cleartext.py
+++ src/solvers/cleartext.py
@@ -31,34 +31,63 @@ def solve(args):
     
     # AMQP
 
-    hosts = scan.sub_hosts.get("87733")
-    vuln = {}    
-    nm = nmap.PortScanner()
-    for host in hosts:
-        try:
-            ip = host.split(":")[0]
-            port = host.split(":")[1]
-            nm.scan(ip, port, arguments=f'--script amqp-info')
-            
-            if ip in nm.all_hosts():
-                nmap_host = nm[ip]
-                if 'tcp' in nmap_host and int(port) in nmap_host['tcp']:
-                    tcp_info = nmap_host['tcp'][int(port)]
-                    if 'script' in tcp_info and 'amqp-info' in tcp_info['script']:
+    try:
+        hosts = scan.sub_hosts.get("87733")
+        vuln = {}    
+        nm = nmap.PortScanner()
+        for host in hosts:
+            try:
+                ip = host.split(":")[0]
+                port = host.split(":")[1]
+                nm.scan(ip, port, arguments=f'--script amqp-info')
+                
+                if ip in nm.all_hosts():
+                    nmap_host = nm[ip]
+                    if 'tcp' in nmap_host and int(port) in nmap_host['tcp']:
+                        tcp_info = nmap_host['tcp'][int(port)]
+                        if 'script' in tcp_info and 'amqp-info' in tcp_info['script']:
 
-                        amqpinfo = tcp_info['script']['amqp-info']
+                            amqpinfo = tcp_info['script']['amqp-info']
 
-                        mech = None
+                            mech = None
 
-                        for line in amqpinfo.splitlines():
-                            if "mechanisms:" in line:
-                                mech = line.split(":")[1].strip()
-                        if mech:
-                            vuln[host] = mech
+                            for line in amqpinfo.splitlines():
+                                if "mechanisms:" in line:
+                                    mech = line.split(":")[1].strip()
+                            if mech:
+                                vuln[host] = mech
 
-        except Exception as e: pass #print(e)
+            except Exception as e: pass #print(e)
+        
+        if len(vuln) > 0:
+            print("AMQP Plain Authentication Mechanism Detected:")
+            for key, value in vuln.items():
+                print(f"{key}: {value}")
+    except: pass
     
-    if len(vuln) > 0:
-        print("AMQP Plain Authentication Mechanism Detected:")
-        for key, value in vuln.items():
-            print(f"{key}: {value}")
+    # Telnet
+
+    try:
+        hosts = scan.sub_hosts.get("42263")
+        vuln = []   
+        nm = nmap.PortScanner()
+        for host in hosts:
+            try:
+                ip = host.split(":")[0]
+                port = host.split(":")[1]
+                nm.scan(ip, port, arguments=f'-sV')
+                
+                if ip in nm.all_hosts():
+                    nmap_host = nm[ip]
+                    for port, details in nm[host]['tcp'].items():
+                        service_name = details['name']
+                        if service_name.lower() == 'telnet':
+                            vuln.append(host)
+                        
+            except Exception as e: pass #print(e)
+        
+        if len(vuln) > 0:
+            print("Unencrypted Telnet Detected:")
+            for value in vuln:
+                print(f"{value}")
+    except: pass
