a90d40958de273f9a64709904925d8fd9290cf6d
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-12 12:29:28 +0300

    cleartext

--- src/solvers/cleartext.py
+++ src/solvers/cleartext.py
@@ -15,25 +15,11 @@ def helper_parse(subparser):
     parser_task1 = subparser.add_parser(str(code), help="Cleartext Protocol Detected")
     parser_task1.add_argument("-f", "--file", type=str, required=True, help="JSON file name")
     parser_task1.set_defaults(func=solve)
-    
-
-def solve(args):
-    l= logger.setup_logging(args.verbose)
-    scan: GroupNessusScanOutput = find_scan(args.file, code)
-    if not scan and not args.ignore_fail: 
-        print("No id found in json file")
-        return
-    
-    if args.config:
-        with open(args.config, "rb") as f:
-            data = tomllib.load(f)
-    
-    hosts = scan.hosts
-    
-    # AMQP
-
+   
+def solve_amqp(scan: GroupNessusScanOutput):
     try:
         hosts = scan.sub_hosts.get("87733")
+        if not hosts: return
         vuln = {}    
         nm = nmap.PortScanner()
         for host in hosts:
@@ -65,11 +51,11 @@ def solve(args):
             for key, value in vuln.items():
                 print(f"{key}: {value}")
     except: pass
-    
-    # Telnet
 
+def solve_telnet(scan: GroupNessusScanOutput):
     try:
         hosts = scan.sub_hosts.get("42263")
+        if not hosts: return
         vuln = []   
         nm = nmap.PortScanner()
         for host in hosts:
@@ -90,6 +76,72 @@ def solve(args):
             for value in vuln:
                 print(f"{value}")
     except: pass
+
+def solve_basic_http(scan: GroupNessusScanOutput):
+    try:
+        hosts = scan.sub_hosts.get("98615")
+        if not hosts: return
+        vuln = []
+        for host in hosts:
+            ip = host.split(":")[0]
+            port  = host.split(":")[1]
+            try:
+                response = requests.get(f"http://{host}", timeout=5)
+                if response.status_code == 401 and "WWW-Authenticate" in response.headers:
+                    vuln.append(host)
+
+            except: pass
+            
+        if len(vuln) > 0:
+            print("Basic Authentication Without HTTPS Detected:")
+            for value in vuln:
+                print(f"{value}")
+    except: pass
+
+def solve_ftp(scan: GroupNessusScanOutput):
+    try:
+        hosts = scan.sub_hosts.get("34324")
+        if not hosts: return
+        vuln = []
+        for host in hosts:
+            ip = host.split(":")[0]
+            port  = int(host.split(":")[1])
+
+            ftp = FTP()
+            ftp.connect(ip, port)
+            try:
+                l = ftp.login()
+                if "230" in l:
+                    vuln.append(host)
+
+            except error_perm as e:
+                if "530" in str(e):
+                    vuln.append(host)
+            except Exception: pass
+                
+        if len(vuln) > 0:
+            print("FTP Supports Cleartext Authentication:")
+            for value in vuln:
+                print(f"{value}")
+    except: pass
+
+def solve(args):
+    l= logger.setup_logging(args.verbose)
+    scan: GroupNessusScanOutput = find_scan(args.file, code)
+    if not scan and not args.ignore_fail: 
+        print("No id found in json file")
+        return
+    
+    if args.config:
+        with open(args.config, "rb") as f:
+            data = tomllib.load(f)
+    
+
+    solve_amqp(scan)
+    solve_telnet(scan)
+    solve_basic_http(scan)
+    solve_ftp(scan)
+
     
     """
     # SMTP (TODO)
@@ -115,47 +167,6 @@ def solve(args):
     except Exception as e: print(e)
     """
     # Basic Authentication Without HTTPS
-    try:
-        hosts = scan.sub_hosts.get("98615")
-        vuln = []
-        for host in hosts:
-            ip = host.split(":")[0]
-            port  = host.split(":")[1]
-            try:
-                response = requests.get(f"http://{host}", timeout=5)
-                if response.status_code == 401 and "WWW-Authenticate" in response.headers:
-                    vuln.append(host)
 
-            except: pass
-            
-        if len(vuln) > 0:
-            print("Basic Authentication Without HTTPS Detected:")
-            for value in vuln:
-                print(f"{value}")
-    except: pass
     
     # FTP Supports Cleartext Authentication
-    try:
-        hosts = scan.sub_hosts.get("34324")
-        vuln = []
-        for host in hosts:
-            ip = host.split(":")[0]
-            port  = int(host.split(":")[1])
-
-            ftp = FTP()
-            ftp.connect(ip, port)
-            try:
-                l = ftp.login()
-                if "230" in l:
-                    vuln.append(host)
-
-            except error_perm as e:
-                if "530" in str(e):
-                    vuln.append(host)
-            except Exception: pass
-                
-        if len(vuln) > 0:
-            print("FTP Supports Cleartext Authentication:")
-            for value in vuln:
-                print(f"{value}")
-    except: pass
