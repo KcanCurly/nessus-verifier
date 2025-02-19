34ca5d2edfa96ced132c46fc3739fe36bf6aca7c
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-19 22:47:27 +0300

    smb

--- src/services/smb.py
+++ src/services/smb.py
@@ -56,8 +56,9 @@ def nullguest_single(single_progress: Progress, single_task_id: TaskID, console:
                 single_progress.update(single_task_id, status = "[red]SMB connect failed[/red]",advance=1)
             else:
                 shares = conn.listShares(timeout=timeout)
-                try:
-                    for share in shares:
+
+                for share in shares:
+                    try:
                         files = conn.listPath(share.name, "/")
                         
                         null_vuln[share.name] = []
@@ -65,7 +66,7 @@ def nullguest_single(single_progress: Progress, single_task_id: TaskID, console:
                         for file in files:
                             if file.filename == "." or file.filename == "..": continue
                             null_vuln[share.name].append(file.filename)
-                except: pass
+                    except: pass
                 single_progress.update(single_task_id, status = "[green]Process finished[/green]",advance=1)
         except Exception as e:
                 single_progress.update(single_task_id, status = f"[red]Failed {e}[/red]",advance=1)
