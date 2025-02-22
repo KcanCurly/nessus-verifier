53695ce65b8860526fefc58216e6eea81f765591
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-22 21:21:46 +0300

    mongo

--- src/services/mongodb.py
+++ src/services/mongodb.py
@@ -27,6 +27,28 @@ def post_nv(l: list[str], output: str = None, threads: int = 10, timeout: int = 
 def post_console(args):
     post_nv(get_hosts_from_file(args.file))
 
+
+def unauth_nv(l: list[str], output: str = None, threads: int = 10, timeout: int = 3, verbose: bool = False, disable_visual_on_complete: bool = False):
+    vuln = []
+    
+    for host in l:
+        try:
+            ip = host.split(":")[0]
+            port = host.split(":")[1]
+            client = MongoClient(ip, int(port))
+            dbs = client.list_databases()
+            vuln.append(host)
+
+        except Exception as e: print(f"Error for {host}:", e)
+    
+    if len(vuln) > 0:
+        print("MongoDB Unauthenticated Access:")
+        for v in vuln:
+            print(f"    {v}")
+
+def unauth_console(args):
+    unauth_nv(get_hosts_from_file(args.file))
+
 def version_nv(l: list[str], output: str = None, threads: int = 10, timeout: int = 3, verbose: bool = False, disable_visual_on_complete: bool = False):
     versions = {}
     
@@ -77,7 +99,7 @@ def main():
     parser_version.add_argument("-v", "--verbose", action="store_true", help="Enable verbose")
     parser_version.set_defaults(func=version_console)
     
-    """
+
     parser_unauth = subparsers.add_parser("unauth", help="Checks if unauthenticated access is allowed")
     parser_unauth.add_argument("-f", "--file", type=str, required=True, help="input file name")
     parser_unauth.add_argument("--threads", default=10, type=int, help="Number of threads (Default = 10)")
@@ -85,8 +107,8 @@ def main():
     parser_unauth.add_argument("--disable-visual-on-complete", action="store_true", help="Disables the status visual for an individual task when that task is complete, this can help on keeping eye on what is going on at the time")
     parser_unauth.add_argument("--only-show-progress", action="store_true", help="Only show overall progress bar")
     parser_unauth.add_argument("-v", "--verbose", action="store_true", help="Enable verbose")
-    parser_unauth.set_defaults(func=unpassworded_console)
-    
+    parser_unauth.set_defaults(func=unauth_console)
+    """
     parser_brute = subparsers.add_parser("brute", help="Bruteforce")
     parser_brute.add_argument("-f", "--file", type=str, required=True, help="input file name")
     parser_brute.add_argument("-cf", "--credential-file", type=str, help="Credential file")
