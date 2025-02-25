4394c9385e0f0ca7abe51fd6e5131a789f508101
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-25 13:44:09 +0300

    zookeeper

--- src/services/zookeeper.py
+++ src/services/zookeeper.py
@@ -2,55 +2,49 @@ import argparse
 import subprocess
 import re
 from src.utilities.utilities import get_hosts_from_file, get_classic_console
-import nmap
-import traceback
 
 def enum_nv(l: list[str], output: str = None, threads: int = 10, verbose: bool = False):
+    versions = {}
+
 
     result = ", ".join(l)
     vuln = {}
     try:
-        command = ["msfconsole", "-q", "-x", f"color false; use auxiliary/gather/zookeeper_info_disclosure; set RHOSTS {result}; set THREADS {str(threads)}; run; exit"]
+        command = ["msfconsole", "-q", "-x", f"color false; use auxiliary/gather/zookeeper_info_disclosure; set RHOSTS {result}; run; exit"]
         result = subprocess.run(command, text=True, capture_output=True)
         host_start = r"\[\*\] (.*)\s+ - Using a timeout of"
+        zookeeper_version = r"zookeeper.version=(.*),"
         
         host = ""
         
         for line in result.stdout.splitlines():
-            matches = re.search(host_start, line)
-            if matches:
-                host = matches.group(1)
-                print(host)
-            
-            pass
-        
-        """
-        for m in matches:
-            if m[1] not in vuln:
-                vuln[m[1]] = set()
-            vuln[m[1]].add(m[0])
-        """
-            
-
+            try:
+                matches = re.search(host_start, line)
+                if matches:
+                    host = matches.group(1)
+                    continue
+                
+                matches = re.search(zookeeper_version, line)
+                if matches:
+                    ver = matches.group(1).split("-")[0]
+                    if ver not in vuln:
+                        vuln[ver] = set()
+                    vuln[ver].add(host)
+            except: pass
         
             
     except:pass
-    """
-    if len(vuln) > 0:
-        print("TFTP files were found:")
-        for k,v in vuln.items():
-            print(f"{k}:69:")
+
+    if len(versions) > 0:
+        versions = dict(sorted(versions.items(), reverse=True))
+        print("Apache Zookeeper Versions:")
+        for k,v in versions.items():
+            print(f"{k}:2181:")
             for a in v:
                 print(f"    {a}")
         
-        if output:
-            with open(output, "a") as file:
-                print("TFTP files were found:", file=file)
-                for k,v in vuln.items():
-                    print(f"{k}:69:", file=file)
-                    for a in v:
-                        print(f"    {a}", file=file)
-    """
+
+
         
 
 def enum_console(args):
@@ -61,9 +55,8 @@ def main():
     parser = argparse.ArgumentParser(description="Zookeeper module of nessus-verifier.")
     subparsers = parser.add_subparsers(dest="command")
     
-    brute_parser = subparsers.add_parser("enum", help="Run enumeration on zookeeper host")
+    brute_parser = subparsers.add_parser("enum", help="Run enumeration on zookeeper host") # Don't want to have threads because we parse line by line, multiple threads can break the parsing logic
     brute_parser.add_argument("-f", "--file", type=str, required=False, help="Path to a file containing a list of hosts, each in 'ip:port' format, one per line.")
-    brute_parser.add_argument("--threads", type=int, default=10, help="Threads (Default = 10).")
     brute_parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
     brute_parser.set_defaults(func=enum_console)
 
