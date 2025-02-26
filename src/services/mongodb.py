a8341a15c1f92b65fc32682a874d9582e3b7cdbc
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-02-26 13:36:18 +0300

    mongodb

--- src/services/mongodb.py
+++ src/services/mongodb.py
@@ -76,7 +76,7 @@ def version_nv(l: list[str], output: str = None, threads: int = 10, timeout: int
                 if version not in versions:
                     versions[version] = set()
                 versions[version].add(host)  
-        except:pass
+        except Exception as e: print(f"Error for {host}: {e}")
                     
     versions = dict(sorted(versions.items(), reverse=True))
     if len(versions) > 0:       
