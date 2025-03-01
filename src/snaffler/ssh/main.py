ef2221e667ed01d1fa5f9e3ad94bc04111f546d2
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-01 20:26:09 +0300

    ssh

--- src/snaffler/ssh/main.py
+++ src/snaffler/ssh/main.py
@@ -110,11 +110,15 @@ async def process_directory(sftp: asyncssh.SFTPClient, host:str, username:str, r
             item_path = f"{remote_path if remote_path != "/" else ""}/{d.filename}"
             if await sftp.isdir(item_path):
                 if not rules.enum_directory(item_path)[0]:continue
-                print("  " * depth + f"[D] {item_path}")
+                if verbose: print("  " * depth + f"[D] {item_path}")
                 tasks.append(process_directory(sftp, host, username, rules, verbose, item_path, depth=depth+1))
                 # await process_directory(sftp, host, username, rules, verbose, item_path, depth=depth+1)
             elif await sftp.isfile(item_path):
-                print("  " * depth + f"[F] {item_path}")
+                enum_file = rules.enum_file(item_path)
+                if not enum_file[0]:continue
+                for b,c in enum_file[1].items():
+                    print(f"{host} - {username} => {item_path} - {b.name} - {c}")
+                if verbose: print("  " * depth + f"[F] {item_path}")
         await asyncio.gather(*tasks)
     except Exception as e: print(e)
     
