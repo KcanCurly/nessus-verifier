44e81c64cf41b5da552571d46606e515f2badcf5
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-01 14:49:45 +0300

    ssh

--- src/snaffler/ssh/main.py
+++ src/snaffler/ssh/main.py
@@ -134,5 +134,4 @@ def main():
                 single_task_id = single_progress.add_task("single", start=False, host=host, status="status", total=1)
                 future = executor.submit(list_remote_directory, sftp, host, username, rules, args.verbose, "/")
                 futures.append(future)
-            for a in as_completed(futures):
-                overall_progress.update(overall_task_id, advance=1)
+
