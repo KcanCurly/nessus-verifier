504c9e220a424369f87968dfb9dfab59a89ccb46
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-03 20:52:39 +0300

    smb

--- src/snaffler/smb/main.py
+++ src/snaffler/smb/main.py
@@ -15,7 +15,8 @@ def list_files_recursively(conn, share, directory="*"):
             if filename in [".", ".."]:  # Skip current and parent directory links
                 continue
 
-            full_path = f"{directory + "\\" if directory != "*" else ""}{filename}"
+            if "\\" not in directory: full_path = filename
+            else:  full_path = f"{directory}\\{filename}"
 
             if file.is_directory():
                 print(f"[DIR] {full_path}")
