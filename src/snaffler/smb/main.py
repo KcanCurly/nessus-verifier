4a514d5fb13aeef2704b9757f3070000ac6d46d9
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-03 22:40:56 +0300

    smb

--- src/snaffler/smb/main.py
+++ src/snaffler/smb/main.py
@@ -34,7 +34,7 @@ def list_files_recursively(conn, share, rules, directory="*"):
             else:
                 if file.get_filesize() / 1024 > MAX_FILE_SIZE_MB: continue
                 enum_file = rules.enum_file(full_path)
-                if not enum_file[0] or file.get_filesize() / 1024 > MAX_FILE_SIZE_MB or not can_read_file(conn, full_path):continue
+                if not enum_file[0] or file.get_filesize() / 1024 > MAX_FILE_SIZE_MB or not can_read_file(conn, share, full_path):continue
                 print(f"[FILE] {full_path}")
 
     except Exception as e:
