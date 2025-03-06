4108946ac9f1791c13f319645d28d431dfb83e5d
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-06 21:49:43 +0300

    ssh

--- src/snaffler/ssh/main.py
+++ src/snaffler/ssh/main.py
@@ -15,28 +15,20 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from src.utilities.utilities import get_classic_single_progress, get_classic_overall_progress, get_classic_console, get_hosts_from_file
 import asyncio
 import os
+import multiprocessing
 import threading
 
 MAX_FILE_SIZE_MB = 100
 MAX_LINE_CHARACTER = 300
 
-history_lock = threading.Lock()
-output_lock = threading.Lock()
+history_lock = multiprocessing.Lock()
+output_lock = multiprocessing.Lock()
 output_file = ""
 output_file_path = ""
 module_console = None
 history_dict = dict[str, set]()
 
 semaphore = asyncio.Semaphore(1)
-
-
-def multithread_export_print(console: Console):
-    return
-    with output_lock:
-        a = console.export_text(styles=True)
-        with open(output_file, "a") as f:
-            f.write(a)
-
 
 async def can_read_file(sftp, path):
     """Attempts to open a remote file in read mode to check permissions."""
@@ -60,7 +52,6 @@ async def get_file_size_mb(sftp, path, error, live):
         return None
 
 def print_finding(console, host:str, username:str, rule:SnaffleRule, path:str, findings:list[str]):
-    return
     console.print(f"[{rule.triage.value}]\[{host}]\[{username}]\[{rule.importance}]\[{rule.name}][/{rule.triage.value}][white] | {path} | {findings}[/white]")
 
 async def process_file(sftp: asyncssh.SFTPClient, host:str, username:str, rules: SnafflerRuleSet, verbose, path, live:Live, error):
@@ -80,13 +71,14 @@ async def process_file(sftp: asyncssh.SFTPClient, host:str, username:str, rules:
                 if a[0]:
                     for b,c in a[1].items():
                         print_finding(live.console, host, username, b, path, c)
-                        #print_finding(module_console, host, username, b, path, c)
-                    multithread_export_print(module_console)
+                        print_finding(module_console, host, username, b, path, c)
+
     except Exception as e: 
         if error: live.console.print("Process File Error:", e)
 
 async def process_directory(sftp: asyncssh.SFTPClient, host:str, username:str, rules: SnafflerRuleSet, verbose, live:Live, error, remote_path=".", depth=0):
     try:
+        tasks = []
         dir = await sftp.readdir(remote_path)
         for d in dir:
             if d.filename == "." or d.filename == "..":continue
@@ -98,6 +90,10 @@ async def process_directory(sftp: asyncssh.SFTPClient, host:str, username:str, r
                 await process_directory(sftp, host, username, rules, verbose, live, error, item_path, depth=depth+1)
             elif await sftp.isfile(item_path):
                 if item_path == output_file_path: continue
+                with history_lock:
+                    if item_path in history_dict[host]:
+                        if verbose: live.console.print(f"[F] | Already processed, skipping | {item_path}")
+                        continue
                 enum_file = rules.enum_file(item_path)
                 if verbose: live.console.print(f"[F] | Processing | {item_path}")
                 if not enum_file[0]:
@@ -111,17 +107,13 @@ async def process_directory(sftp: asyncssh.SFTPClient, host:str, username:str, r
                     if verbose: live.console.print(f"[F] | Read Failed | {item_path}")
                     continue
 
-                """
+
                 with history_lock:
-                    if item_path in history_dict[host]:
-                        if verbose: live.console.print(f"[F] | Already processed, skipping | {item_path}")
-                        continue
                     history_dict[host].add(item_path)
-                """
+
                 for b,c in enum_file[1].items():
                     print_finding(live.console, host, username, b, item_path, c)
-                    #print_finding(module_console, host, username, b, item_path, c)
-                multithread_export_print(module_console)
+                    print_finding(module_console, host, username, b, item_path, c)
                 if verbose: live.console.print(f"[F] {item_path}")
                 await process_file(sftp, host, username, rules, verbose, item_path, live, error)
                 #tasks.append(process_file(sftp, host, username, rules, verbose, item_path, live, error))
@@ -159,7 +151,7 @@ async def main2():
     args = parser.parse_args()
     overall_progress = get_classic_overall_progress()
     overall_task_id = overall_progress.add_task("", start=False, modulename="SSH Snaffle")
-    console = Console(force_terminal=True)    
+    console = Console(force_terminal=True)
     global semaphore
     semaphore = asyncio.Semaphore(args.thread)
     
@@ -188,6 +180,71 @@ async def main2():
             
             await asyncio.gather(*tasks)  # Wait for all tasks to complete
 
+
+async def process_host2(data):
+    hostname = data["hostname"]
+    port = data["port"]
+    username = data["username"]
+    password = data["password"]
+    verbose = data["verbose"]
+    live = data["live"]
+    rules = data["rules"]
+    error = data["error"]
+    """Main function to process a single SSH host asynchronously."""
+    async with semaphore:
+        try:
+            async with await connect_ssh(hostname, port, username, password) as conn:
+                if verbose: live.console.print(f"Connected to {hostname}:{port}")
+                history_dict[f"{hostname}:{port}"] = set()
+                sftp = await conn.start_sftp_client()
+                await process_directory(sftp, f"{hostname}:{port}", username, rules, verbose, live, error, "/")
+                
+        except Exception as e:
+            print(f"Error processing {hostname}: {e}")
+
+
+
+async def main2():
+    parser = argparse.ArgumentParser(description="Snaffle via SSH.")
+    parser.add_argument("-f", "--file", type=str, required=True, help="Input file name, format is 'host:port => username:password'")
+    parser.add_argument("-o", "--output", type=str, required=True, help="Output File.")
+    parser.add_argument("--show-importance", type=int, default=0, help="Print only snaffles that is above given importance level, does NOT affect output to file.")
+    parser.add_argument("-t","--thread", default=10, type=int, help="Number of threads (Default = 10)")
+    parser.add_argument("--timeout", default=5, type=int, help="Timeout in seconds (Default = 5)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose")
+    parser.add_argument("-e", "--error", action="store_true", help="Prints errors")
+    
+    args = parser.parse_args()
+    overall_progress = get_classic_overall_progress()
+    overall_task_id = overall_progress.add_task("", start=False, modulename="SSH Snaffle")
+    console = Console(force_terminal=True)
+    global semaphore
+    semaphore = asyncio.Semaphore(args.thread)
+    
+    global output_file, output_file_path, module_console
+    module_console = Console(force_terminal=True, record=True, quiet=True)    
+    
+
+    output_file = args.output
+    with open(output_file, "w") as f:
+        output_file_path = os.path.abspath(f.name)
+        module_console = Console(force_terminal=True, record=True, file=f)    
+        rules = SnafflerRuleSet.load_default_ruleset()
+
+
+        with Live(overall_progress, console=console) as live:
+            overall_progress.update(overall_task_id, total=len(get_hosts_from_file(args.file)), completed=0)
+            overall_progress.start_task(overall_task_id)
+            l = [{}]
+            for entry in get_hosts_from_file(args.file):
+                host, cred = entry.split(" => ")
+                ip, port = host.split(":")
+                username, password = cred.split(":")
+                l.append({"hostname": host, "username": username, "password": password, "port": port, "verbose": args.verbose, "error": args.error, "live": live, "rules": rules})
+                # tasks.append(asyncio.create_task(process_host(ip, port, username, password, rules, args.verbose, live, args.error)))
+            with multiprocessing.Pool(processes=args.threads) as pool:
+                results = pool.map(process_host2, l)  # Run SSH connections in parallel
+            
 
 
 
