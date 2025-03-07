944c0d25d0ea8d84ac282441e857559647476088
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-07 12:36:21 +0300

    ssh

--- src/snaffler/ssh/main.py
+++ src/snaffler/ssh/main.py
@@ -92,6 +92,8 @@ def process_directory(sftp: paramiko.SFTPClient, host:str, username:str, rules: 
                     if item_path == output_file_path: continue
 
                     with history_lock:
+                        print(history_dict)
+                        print(f"{host in history_dict}")
                         if item_path in history_dict[host]:
                             if verbose: console.print(f"[F] {host} | {username} | Already processed, skipping | {item_path}")
                             continue
