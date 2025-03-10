e63b38c4d510f6a404f1f79fcd92563376a4a5f2
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-10 20:17:03 +0300

    services

--- src/services/idrac.py
+++ src/services/idrac.py
@@ -1,15 +1,7 @@
-import argparse
-import subprocess
-import re
-from impacket.smbconnection import SMBConnection
-from smb import SMBConnection as pysmbconn
-from src.utilities.utilities import get_classic_single_progress, get_classic_overall_progress, get_classic_console, get_hosts_from_file
+from src.utilities.utilities import get_classic_overall_progress, get_classic_console, get_hosts_from_file
 from rich.live import Live
-from rich.progress import Progress, TaskID
 from rich.console import Console
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from rich.console import Group
-from rich.panel import Panel
 import requests
 
 class Version_Vuln_Data():
@@ -17,7 +9,7 @@ class Version_Vuln_Data():
         self.host = host
         self.version = version
 
-def version_single(single_progress: Progress, single_task_id: TaskID, console: Console, host: str, output: str, timeout: int, verbose: bool):
+def version_single(console: Console, host: str, output: str, timeout: int, verbose: bool):
     try:
         try:
             resp = requests.get(f"https://{host}/sysmgmt/2015/bmc/info", allow_redirects=True, verify=False, timeout=timeout)
@@ -30,32 +22,28 @@ def version_single(single_progress: Progress, single_task_id: TaskID, console: C
         return Version_Vuln_Data(host, version)
     except:return
 
-def version_nv(l: list[str], output: str = None, threads: int = 10, timeout: int = 3, verbose: bool = False, disable_visual_on_complete: bool = False, only_show_progress: bool = False):
+def version_nv(l: list[str], output: str, threads: int, timeout: int, verbose: bool ):
     versions = {}
     
     overall_progress = get_classic_overall_progress()
-    single_progress = get_classic_single_progress()
+
     overall_task_id = overall_progress.add_task("", start=False, modulename="IDRAC Version")
     console = get_classic_console(force_terminal=True)
     
-    progress_group = Group(
-        Panel(single_progress, title="IDRAC Version", expand=False),
-        overall_progress,
-    ) if not only_show_progress else Group(overall_progress)
-    
-    with Live(progress_group, console=console):
+
+    with Live(overall_progress, console=console):
         overall_progress.update(overall_task_id, total=len(l), completed=0)
         overall_progress.start_task(overall_task_id)
         futures = []
         results: list[Version_Vuln_Data] = []
         with ThreadPoolExecutor(threads) as executor:
             for host in l:
-                single_task_id = single_progress.add_task("single", start=False, host=host, status="status", total=1)
-                future = executor.submit(version_single, single_progress, single_task_id, console, host, output, timeout, verbose)
+                future = executor.submit(version_single, console, host, output, timeout, verbose)
                 futures.append(future)
             for a in as_completed(futures):
                 overall_progress.update(overall_task_id, advance=1)
                 results.append(a.result())
+                
     for r in results:
         if not r: continue
         if r.version not in versions:
@@ -70,30 +58,16 @@ def version_nv(l: list[str], output: str = None, threads: int = 10, timeout: int
                 print(f"    {v}")
 
 def version_console(args):
-    version_nv(get_hosts_from_file(args.file), threads=args.threads, timeout=args.timeout, verbose=args.verbose, disable_visual_on_complete=args.disable_visual_on_complete, only_show_progress=args.only_show_progress)
+    version_nv(get_hosts_from_file(args.file), threads=args.threads, timeout=args.timeout, verbose=args.verbose)
 
-def all(args):
-    version_console(args)
     
-def main():    
-    parser = argparse.ArgumentParser(description="IDRAC module of nessus-verifier.")
+def helper_parse(commandparser):    
+    parser_task1 = commandparser.add_parser("idrac")
+    subparsers = parser_task1.add_subparsers(dest="command")
     
-    subparsers = parser.add_subparsers(dest="command")  # Create subparsers
-    
-    parser_all = subparsers.add_parser("all", help="Runs all modules")
+    parser_all = subparsers.add_parser("version", help="Checks idrac version")
     parser_all.add_argument("-f", "--file", type=str, required=True, help="input file name")
     parser_all.add_argument("--threads", default=10, type=int, help="Number of threads (Default = 10)")
     parser_all.add_argument("--timeout", default=5, type=int, help="Timeout in seconds (Default = 5)")
-    parser_all.add_argument("--disable-visual-on-complete", action="store_true", help="Disables the status visual for an individual task when that task is complete, this can help on keeping eye on what is going on at the time")
-    parser_all.add_argument("--only-show-progress", action="store_true", help="Only show overall progress bar")
     parser_all.add_argument("-v", "--verbose", action="store_true", help="Enable verbose")
-    parser_all.set_defaults(func=all)
-    
-    parser_version = subparsers.add_parser("smbv1", help="Checks Version")
-    parser_version.add_argument("-f", "--file", type=str, required=True, help="input file name")
-    parser_version.add_argument("--threads", default=10, type=int, help="Number of threads (Default = 10)")
-    parser_version.add_argument("--timeout", default=5, type=int, help="Timeout in seconds (Default = 5)")
-    parser_version.add_argument("--disable-visual-on-complete", action="store_true", help="Disables the status visual for an individual task when that task is complete, this can help on keeping eye on what is going on at the time")
-    parser_version.add_argument("--only-show-progress", action="store_true", help="Only show overall progress bar")
-    parser_version.add_argument("-v", "--verbose", action="store_true", help="Enable verbose")
-    parser_version.set_defaults(func=version_console)
+    parser_all.set_defaults(func=version_console)
