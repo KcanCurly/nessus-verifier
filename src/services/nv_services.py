1159c006e50d58674fd92ec2edb537b665555306
Kerim Can Karaduman <karadumankcan@gmail.com>
2025-03-23 23:02:11 +0300

    daytime

--- src/services/nv_services.py
+++ src/services/nv_services.py
@@ -1,5 +1,5 @@
 import argparse
-from src.services import asterisk, discard, dns, echo, finger, ftp, ident, ldap, mongodb, ms_exchange, mssql, netstat, qotd, smb, ssh, snmp, nfs, rpc, smtp, snmp, systat, telnet, tftp, time, mssql, idrac, zookeeper, postgresql, mysql
+from src.services import asterisk, daytime, discard, dns, echo, finger, ftp, ident, ldap, mongodb, ms_exchange, mssql, netstat, qotd, smb, ssh, snmp, nfs, rpc, smtp, snmp, systat, telnet, tftp, time, mssql, idrac, zookeeper, postgresql, mysql
 
 service_list = [
     dns,
@@ -30,6 +30,7 @@ service_list = [
     qotd,
     echo,
     discard,
+    daytime,
 ]
 
 
