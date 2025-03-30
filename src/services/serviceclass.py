693243173ec063394f8dc7e74bc1d159f3472acc
kcancurly <karadumankcan@gmail.com>
2025-03-30 18:58:55 +0300

    update

--- src/services/serviceclass.py
+++ src/services/serviceclass.py
@@ -7,8 +7,9 @@ class BaseServiceClass():
         parser_task1 = commandparser.add_parser(self.name)
         subparsers = parser_task1.add_subparsers(dest="command")
         self.subparser = subparsers
+        for subservice in self.subservices:
+            subservice.helper_parse(self.subparser)
 
     def register_subservice(self, subservice):
         subservice._set_parent(self)
-        subservice.helper_parse(self.subparser)
         self.subservices.append(subservice)
