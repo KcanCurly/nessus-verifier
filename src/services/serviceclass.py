36d4fe18f4ada75389e7ddcc8a102e8c56b43b94
kcancurly <karadumankcan@gmail.com>
2025-03-30 18:56:17 +0300

    update

--- src/services/serviceclass.py
+++ src/services/serviceclass.py
@@ -1,16 +1,14 @@
-from src.services.servicesubclass import BaseSubServiceClass as base_subservice
-
 class BaseServiceClass():
     def __init__(self, name: str) -> None:
         self.name = name
-        self.subservices: list[base_subservice] = []
+        self.subservices= []
 
     def helper_parse(self, commandparser):
         parser_task1 = commandparser.add_parser(self.name)
         subparsers = parser_task1.add_subparsers(dest="command")
         self.subparser = subparsers
 
-    def register_subservice(self, subservice: base_subservice):
+    def register_subservice(self, subservice):
         subservice._set_parent(self)
         subservice.helper_parse(self.subparser)
         self.subservices.append(subservice)
