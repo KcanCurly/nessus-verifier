63e36bc55b8f3bb914377743f4b4cf87fde3f21a
kcancurly <karadumankcan@gmail.com>
2025-08-15 23:10:09 +0300

    u

--- src/solvers/hpilo.py
+++ src/solvers/hpilo.py
@@ -1,5 +1,8 @@
-from src.utilities.utilities import Host, Version_Vuln_Host_Data, error_handler, get_url_response, get_default_context_execution
+from src.utilities.utilities import Host, Version_Vuln_Host_Data, error_handler, get_cves, get_poc_cve_github_link, get_url_response, get_default_context_execution
 from src.solvers.solverclass import BaseSolverClass
+
+"cpe:2.3:o:hpe:integrated_lights-out_6_firmware:1.55"
+"cpe:2.3:o:hpe:integrated_lights-out_6:1.55"
 
 class HPiLOSolverClass(BaseSolverClass):
     def __init__(self) -> None:
@@ -29,7 +32,7 @@ class HPiLOSolverClass(BaseSolverClass):
     def solve_version(self, hosts, threads, timeout, errors, verbose):
         versions: dict[str, set[Host]] = {}
         results: list[Version_Vuln_Host_Data] = get_default_context_execution("HP iLO Version", threads, hosts, (self.solve_version_single, timeout, errors, verbose))
-
+        all_cves =set()
         for r in results:
             if r.version not in versions:
                 versions[r.version] = set()
@@ -39,13 +42,25 @@ class HPiLOSolverClass(BaseSolverClass):
             self.print_output("Detected HP iLO versions:")
             versions = dict(sorted(versions.items(), reverse=True))
             for key, value in versions.items():
-                """
-                cves = get_cves(f"cpe:2.3:a:grafana:grafana:{key}")
-                if cves: print(f"HP iLO {key} ({", ".join(cves)}):")
-                else: print(f"HP iLO {key}:")
-                """
-                self.print_output(f"HPE {key}:")
+                major, minor = key.split(" - ")
+                cpe1 = f"cpe:2.3:o:hpe:integrated_lights-out_{major}:{minor}"
+                cpe2 = f"cpe:2.3:o:hpe:integrated_lights-out_{major}_firmware:{minor}"
+                cves = get_cves(cpe1)
+                if not cves:
+                    cves = get_cves(cpe2)
+                if cves:
+                    all_cves.update(cves)
+                    self.print_output(f"HPE {key} ({", ".join(cves)}):")
+                else:
+                    self.print_output(f"HPE {key}:")
                 for v in value:
                     self.print_output(f"    {v}")
             self.create_windowcatcher_action()
+
+            for cve in all_cves:
+                links = get_poc_cve_github_link(cve)
+                if links:
+                    self.print_output(f"{cve}:")
+                    for link in links:
+                        self.print_output(link)
                 
