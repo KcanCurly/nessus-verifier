459573875d20f81a45c8ef46516bf5d460f6eb62
kcancurly <karadumankcan@gmail.com>
2025-10-18 11:35:47 +0300

    u

--- src/url/url.py
+++ src/url/url.py
@@ -832,6 +832,7 @@ def main():
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
+        options.set_preference("permissions.default.image", 2)
         driver = webdriver.Firefox(options=options)
 
         with Live(progress_group):
