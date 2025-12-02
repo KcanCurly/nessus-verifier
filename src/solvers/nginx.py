0173a9354c9e97087bb88a1a018e66854e05c0f5
kcancurly <karadumankcan@gmail.com>
2025-12-02 13:29:25 +0300

    u

--- src/solvers/nginx.py
+++ src/solvers/nginx.py
@@ -16,8 +16,8 @@ class NginxSolverClass(BaseSolverClass):
 
         if not self.hosts:
             return
-        if self.is_nv:
-            self.solve_version(self.hosts, args.threads, args.timeout, args.errors, args.verbose)
+
+        self.solve_version(self.hosts, args.threads, args.timeout, args.errors, args.verbose)
 
     @error_handler(["host"])
     def solve_version_single(self, host, timeout, errors, verbose):
