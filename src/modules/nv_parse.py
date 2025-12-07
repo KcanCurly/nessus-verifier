b6a692e188423fbbf117a8ae7ce8967549de4c5a
kcancurly <karadumankcan@gmail.com>
2025-12-07 21:36:22 +0300

    u

--- src/modules/nv_parse.py
+++ src/modules/nv_parse.py
@@ -277,7 +277,9 @@ def write_to_file(l: list[GroupNessusScanOutput], args):
                         plugin_output = get_plugin_output("Web mirroring", z)
                         plugin_output_s = plugin_output.split("+ CGI : ") # type: ignore
                         for p in plugin_output_s[1:]:
-                            print(f"            {p}", file=f)
+                            z = p.strip().split()
+                            for a in z[1:]:
+                                print(f"            {a}", file=f)
                         
     with open(args.output_json_file, "w") as file:
         for v in l:
