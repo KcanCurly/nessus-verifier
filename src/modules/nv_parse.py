9f4bb80f39e94a94963c34b6ff03b6800aa8590d
kcancurly <karadumankcan@gmail.com>
2025-12-07 20:19:12 +0300

    u

--- src/modules/nv_parse.py
+++ src/modules/nv_parse.py
@@ -264,9 +264,15 @@ def write_to_file(l: list[GroupNessusScanOutput], args):
                         plugin_output = get_plugin_output("Web Server Harvested Email Addresses", z)
                         plugin_output_s = plugin_output.split("- ") # type: ignore
                         n = plugin_output_s[0].strip().split()[-1]
-                        print(f"            {n}", file=f)
                         z = plugin_output_s[1].strip().split()
                         for p in z:
+                            if p == "referenced" or p == "from" or p == ":":
+                                continue
+                            if "@" in p:
+                                p = p.replace('\'', "")
+                                p = p.replace(':', "")
+                            elif "/" in p:
+                                p = p.replace('/', "    /")
                             print(f"            {p}", file=f)
                         
     with open(args.output_json_file, "w") as file:
