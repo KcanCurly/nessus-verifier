6b923f93ef1cc4307802354440d1e270df705f90
kcancurly <karadumankcan@gmail.com>
2025-12-07 18:10:42 +0300

    u

--- src/modules/nv_parse.py
+++ src/modules/nv_parse.py
@@ -253,11 +253,7 @@ def write_to_file(l: list[GroupNessusScanOutput], args):
                             print(f"            {p}", file=f)
                     elif key == "Web Server Directory Enumeration":
                         plugin_output = get_plugin_output("Web Server Directory Enumeration", z)
-                        urls =re.findall(r"/S+", plugin_output.strip()) # type: ignore
-                        if "disc" in plugin_output: # type: ignore
-                            print(f"            [NO AUTH]", file=f)
-                        else:
-                            print(f"            [AUTH]", file=f)
+                        urls =re.findall(r"/S+", plugin_output) # type: ignore
                         for p in urls:
                             print(f"            {p}", file=f)
     with open(args.output_json_file, "w") as file:
