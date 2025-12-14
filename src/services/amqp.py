67d0b66497d5e55a52c1d3658e82ab5632afd8ea
kcancurly <karadumankcan@gmail.com>
2025-12-14 19:38:42 +0300

    u

--- src/services/amqp.py
+++ src/services/amqp.py
@@ -47,6 +47,7 @@ class AMQPDefaultCredsSubServiceClass(BaseSubServiceClass):
         anonymous = kwargs.get("anonymous", False)
         try:
             c = amqp.Connection(f"{host.ip}:{host.port}", username, password)
+            print(c.server_capabilities)
             if c.connected:
                 return f"{host.ip}:{host.port}"
         except Exception as e: pass
