99102c16c7d18d3a570a78f70b13ec4104e4217d
kcancurly <karadumankcan@gmail.com>
2025-12-14 12:57:05 +0300

    u

--- src/services/activemq.py
+++ src/services/activemq.py
@@ -63,7 +63,7 @@ class ActiveMQSSLSubServiceClass(BaseSubServiceClass):
             l = Listener()
             conn.set_listener('', l)
             # conn.set_ssl(for_hosts=[(ip, port)])
-            conn.connect("","",wait = True)
+            conn.connect("system","manager",wait = True)
             conn.disconnect()
             time.sleep(0.5)
             if l.z == 1:
