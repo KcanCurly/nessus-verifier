bf7ff20bde33529e71627c11b0c293d4e7187227
kcancurly <karadumankcan@gmail.com>
2025-12-16 16:15:04 +0300

    u

--- src/services/ejabberd.py
+++ src/services/ejabberd.py
@@ -1,19 +1,9 @@
-import socket
-
 import i18n
 from src.utilities.utilities import get_default_context_execution2, error_handler
 from src.services.serviceclass import BaseServiceClass
 from src.services.servicesubclass import BaseSubServiceClass
 from traceback import print_exc
 from ejabberd_python3d.client import EjabberdAPIClient
-
-client = EjabberdAPIClient('localhost','dedaldino','123456')
-users = client.registered_users('localhost')
-# assuming that you have an user registered (the admin) 
-print(users) # [dedaldino]
-client.register('dedaldino3d','localhost','nopassword')
-users = client.registered_users('localhost')
-print(users) # ['dedaldino3d']
 
 class EchoUsageSubServiceClass(BaseSubServiceClass):
     def __init__(self) -> None:
@@ -22,10 +12,10 @@ class EchoUsageSubServiceClass(BaseSubServiceClass):
     def nv(self, hosts, **kwargs):
         super().nv(hosts, kwargs=kwargs)
 
-        results = get_default_context_execution2("Echo Usage", self.threads, hosts, self.single, timeout=self.timeout, errors=self.errors, verbose=self.verbose)
+        results = get_default_context_execution2("Ejabberd Usage", self.threads, hosts, self.single, timeout=self.timeout, errors=self.errors, verbose=self.verbose)
         
         if results:
-            self.print_output(i18n.t('main.usage_title', name='Echo'))
+            self.print_output(i18n.t('main.usage_title', name='Ejabberd'))
             for value in results:
                 self.print_output(f"{value}")
 
