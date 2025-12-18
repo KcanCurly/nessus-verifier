14f61ee455d377f101dcaa7ce7a368c855ddb16a
kcancurly <karadumankcan@gmail.com>
2025-12-18 15:04:14 +0300

    u

--- src/services/apache_tomcat.py
+++ src/services/apache_tomcat.py
@@ -28,7 +28,7 @@ class TomcatBruteforceSubServiceClass(BaseSubServiceClass):
         errors=kwargs.get("errors", False)
         verbose = kwargs.get("verbose", False)
         print(username, password)
-        resp = requests.get(f"http://{host}/manager/status", auth=(username, password))
+        resp = requests.get(f"http://{host}/manager/status", auth=(username, password), headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"})
         print(resp.status_code)
         print(resp.request.headers)
 
