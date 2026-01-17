7fb5bac2f895c0f32beac9160495e997412ce41e
kcancurly <karadumankcan@gmail.com>
2026-01-17 11:30:10 +0300

    u

--- src/modules/nv_parse.py
+++ src/modules/nv_parse.py
@@ -473,9 +473,7 @@ def write_to_file(l: list[GroupNessusScanOutput], args):
                                 print(f"            {m}", file=f)
                     elif key == "LDAP Crafted Search Request Server Information Disclosure":
                         plugin_output = get_plugin_output("LDAP Crafted Search Request Server Information Disclosure", z)
-                        pattern = r"\| (.*)"
-                        matches = re.findall(pattern, plugin_output) # type: ignore
-                        for m in matches:
+                        for m in plugin_output.splitlines(): # type: ignore
                             m = m.strip()
                             print(f"            {m}", file=f)
                     elif key == "HTTP Server Type and Version":
