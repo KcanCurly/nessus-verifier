f6ae3606c32765fff5a48676949612906ee7087a
kcancurly <karadumankcan@gmail.com>
2026-02-17 15:11:45 +0300

    u

--- src/solvers/tomcat.py
+++ src/solvers/tomcat.py
@@ -1,8 +1,6 @@
-import i18n
-from src.utilities.utilities import Version_Vuln_Host_Data, error_handler, get_poc_cve_github_link, get_url_response, get_default_context_execution, get_cves
+from src.services.apache_tomcat import TomcatVersionSubServiceClass
+from src.utilities.utilities import error_handler
 from src.solvers.solverclass import BaseSolverClass
-import re
-from packaging.version import parse
 
 class TomcatSolverClass(BaseSolverClass):
     def __init__(self) -> None:
@@ -18,52 +16,5 @@ class TomcatSolverClass(BaseSolverClass):
 
         if not self.hosts: 
             return
-        versions = {}
-        results: list[Version_Vuln_Host_Data] = get_default_context_execution("Apache Tomcat", args.threads, self.hosts, (self.solve_version_single, args.timeout, args.errors, args.verbose))
-        for r in results:
-            if r.version not in versions:
-                versions[r.version] = set()
-            versions[r.version].add(r.host)
-        all_cves =set()
-        if versions:
-            versions = dict(
-                sorted(versions.items(), key=lambda x: parse(x[0]), reverse=True)
-            )
-            total_cves = []
-            self.print_output(i18n.t('main.version_title', name='Apache Tomcat'))
-            for key, value in versions.items():
-                cves = []
-                if key.startswith("8"): 
-                    self.print_output(f"Apache Tomcat {key} (EOL):")
-                else:
-                    if self.print_cve:
-                        cves = get_cves(f"cpe:2.3:a:apache:tomcat:{key}")
-                    if cves: 
-                        all_cves.update(cves)
-                        self.print_output(f"Apache Tomcat {key} ({", ".join(cves)}):")
-                    else: 
-                        self.print_output(f"Apache Tomcat {key}:")
-                    total_cves.extend(cves)
-                for v in value:
-                    self.print_output(f"    {v}")
-                    
-            self.create_windowcatcher_action()
-            for cve in all_cves:
-                links = get_poc_cve_github_link(cve)
-                if links:
-                    self.print_output(f"{cve}:")
-                    for link in links:
-                        self.print_output(link)
-            self.print_latest_versions()
-    
-    @error_handler(["host"])
-    def solve_version_single(self, host, timeout, errors, verbose):
-        r1 = r"Apache Tomcat\/(\d+\.\d+\.\d+)"
-
-        resp = get_url_response(str(host), timeout=timeout)
-        m = re.search(r1, resp.text, re.MULTILINE) # type: ignore
-        if m: 
-            return Version_Vuln_Host_Data(host, m.group(1))
+        TomcatVersionSubServiceClass().nv(self.hosts, threads=args.threads, timeout=args.timeout, errors=args.errors, verbos=args.verbose, output=self.output, print_latest_version=args.print_latest_version, print_poc=args.print_poc, print_cve=args.print_cve)
             
-
-        
