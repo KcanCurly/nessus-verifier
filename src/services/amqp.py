5f2346253c6207e65f89bfd845637ac2d2a2c5e0
kcancurly <karadumankcan@gmail.com>
2026-02-17 10:24:28 +0300

    u

--- src/services/amqp.py
+++ src/services/amqp.py
@@ -81,9 +81,9 @@ class AMQPDefaultCredsSubServiceClass(BaseSubServiceClass):
                 return host
         except Exception as e: print("Error", e)
 
-class AMQPVersionSubServiceClass(VersionSubService):
+class AMQPVersionSubServiceClass(BaseSubServiceClass):
     def __init__(self) -> None:
-        super().__init__("version", "Checks version", [("RabbitMQ", "rabbitmq")])
+        super().__init__("version", "Checks version")
 
     @error_handler([])
     def nv(self, hosts, **kwargs) -> None:
@@ -91,48 +91,64 @@ class AMQPVersionSubServiceClass(VersionSubService):
         
 
         nm = nmap.PortScanner()
-        results: list[Version_Vuln_Host_Data] = get_default_context_execution2(f"{self.products[0][0]} Version", self.threads, hosts, self.single, nm=nm, timeout=self.timeout, errors=self.errors, verbose=self.verbose)
-        versions = {}
+        results = get_default_context_execution2(f"AMQP Version", self.threads, hosts, self.single, nm=nm, timeout=self.timeout, errors=self.errors, verbose=self.verbose)
+
+
+        version_dict = {}
+
+        filtered = []
 
         for r in results:
-            if r.version not in versions:
-                versions[r.version] = set()
-            versions[r.version].add(r.host)
+            ip_port = r.split("=>")[0].strip()
+            ver = r.split("=>")[1]
+            if ver.strip() == "filtered":
+                filtered.append(ip_port)
+                continue
+            version = ver.split()[3].strip()
+            if version not in version_dict:
+                version_dict[version] = []
+            version_dict[version].append(ip_port)
 
-        if versions:
-            versions = dict(sorted(versions.items(), reverse=True))
-            self.print_output(i18n.t('main.version_title', name=self.products[0][0]))
-            
-            for key, value in versions.items():
-                if "rabbitmq" in key.lower():
-                    _, pure_version = key.rsplit(" ", 1)
-                    self.print_single_version_result("RabbitMQ", value, pure_version, "cpe:2.3:a:vmware:rabbitmq:")
-                else:
-                    self.print_single_version_result(key, value, key, "")
+        cve_set = set()
 
-            self.print_latest_versions()
-            self.print_pocs()
+
+
+        if version_dict:
+            version_dict = dict(sorted(version_dict.items(), reverse=True))
+            self.print_output(i18n.t('main.version_title', name='AMQP'))
+            for version, hosts in version_dict.items():
+                cves = []
+                if "rabbitmq" in version.lower():
+                    _, pure_version, _ = version.split(" ")
+                    if self.should_print_cves:
+                        cves = get_cves("cpe:2.3:a:vmware:rabbitmq:" + pure_version)
+                        cve_set.update(cves)
+                    if cves:
+                        self.print_output(f"RabbitMQ {pure_version}({', '.join(cves)}):")
+                    else:
+                        self.print_output(f"RabbitMQ {pure_version}:")
+                for a in hosts:
+                    self.print_output(f"    {a}")
+
+            self.print_latest_versions("rabbitmq", "RabbitMQ")
+            self.print_pocs(cve_set)
+
+        if filtered:
+            self.print_output(i18n.t('main.filtered_title'))
+            for f in filtered:
+                self.print_output(f"    {f}")
 
 
 
     @error_handler(["host"])
     def single(self, host, **kwargs):
-        nm:nmap.PortScanner = kwargs.get("nm") # type: ignore
-        ip = host.ip
-        port = host.port
-
-        nm.scan(ip, port, arguments=f'--script amqp-info')
-        if ip in nm.all_hosts():
-            nmap_host = nm[ip]
-            if 'tcp' in nmap_host and int(port) in nmap_host['tcp']:
-                tcp_info = nmap_host['tcp'][int(port)]
-                product_name = None
-                version_number = None
-                product_name = tcp_info['product']
-                version_number = tcp_info['version']
-                z = product_name + " " + version_number
-                return Version_Vuln_Host_Data(host, z)
-
+        d = nmap_identify_service_single(host)
+        if d:
+            version = d["version"]
+            if d["state"] == "filtered":
+                return f"{host.ip}:{host.port} => filtered"
+            else:
+                return f"{host.ip}:{host.port} => {version}"
 
 
 class AMQPServiceClass(BaseServiceClass):
