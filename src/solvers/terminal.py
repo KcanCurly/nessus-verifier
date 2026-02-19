7b83510920882678f88f0aae14a9ab658ebc3830
kcancurly <karadumankcan@gmail.com>
2026-02-19 09:02:19 +0300

    u

--- src/solvers/terminal.py
+++ src/solvers/terminal.py
@@ -1,7 +1,8 @@
+import subprocess
 import i18n
 from src.solvers.solverclass import BaseSolverClass
-from src.utilities.utilities import error_handler
-from src.external.rdpy import RDPConfig, LU_ISSUES
+from src.utilities.utilities import error_handler, get_default_context_execution2
+import re
 
 class TerminalSolverClass(BaseSolverClass):
     def __init__(self) -> None:
@@ -10,6 +11,19 @@ class TerminalSolverClass(BaseSolverClass):
         self.output_png_for_action = "terminal.png"
         self.action_title = "Terminal"
 
+    @error_handler(["host"])
+    def solve_single(self, host, timeout, errors, verbose):
+        
+        result = subprocess.run(
+            ["perl", "rdp_check.pl", f"{host.ip}:{host.port}"],
+            capture_output=True,
+            text=True
+        )
+
+        issues = re.findall(r'has issue ([A-Z0-9_]+)', result.stdout)
+        if issues:
+            return {f"{host.ip}:{host.port}": issues}
+
     @error_handler([])
     def solve(self, args):
         self.process_args(args)
@@ -17,23 +31,11 @@ class TerminalSolverClass(BaseSolverClass):
         if not self.hosts: 
             return
 
-        vuln = {}
-
-        for host in self.hosts:
-            #try:
-            rdpc = RDPConfig(host.ip, int(host.port), args.timeout)
-            rdpc.run_tests()
-            if rdpc.issues:
-                vuln[f"{host.ip}:{host.port}"] = []
-                for i in rdpc.issues:
-                    vuln[f"{host.ip}:{host.port}"].append(LU_ISSUES[i])
-
-            #except Exception as e: 
-            #    self._print_exception(f"Error for {host}: {e}")
+        results = get_default_context_execution2("banner grab", args.threads, self.hosts, self.solve_single, timeout=args.timeout)
                 
-        if vuln:
+        if results:
             self.print_output(i18n.t('main.terminal_misconfiguration_title'))
-            for key, value in vuln.items():
+            for key, value in results.items():
                 self.print_output(f"{key}")
                 for v in value:
                     self.print_output(f"    {v}")
